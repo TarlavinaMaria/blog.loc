fbcee6454bdeb902dd7bd6650417ff7e0a3d9272
Мария Тарлавина <tarlavina50@mail.ru>
2025-01-24 19:03:00 +0700

    class work

---
+++ app/controllers/contacts.php
@@ -0,0 +1,9 @@
+<?php
+require_once CORE . "/functions.php";
+
+$title = "Blog/Contacts";
+$contacts = "Some contacts";
+$header = "Contacts";
+
+require_once(VIEWS . "/contacts.tmpl.php");
+?>
--- public/index.php
+++ public/index.php
@@ -1,18 +1,29 @@
 <?php
 
-define("PATH", 'https://blog.loc');
-define("ROOT", dirname(__DIR__));
+define("PATH", 'https://blog.loc'); //domain url
+define("ROOT", dirname(__DIR__)); // root directory
 
-define("APP", ROOT . '/app');
-define("VIEWS", APP . '/views');
-define("COMPONENTS", VIEWS . '/components');
-define("CONTROLLERS", APP . '/controllers');
+define("APP", ROOT . '/app'); // app folder
+define("VIEWS", APP . '/views'); // app/views folder
+define("COMPONENTS", VIEWS . '/components'); // components folder
+define("CONTROLLERS", APP . '/controllers');//app/controllers folder
 
 define("CORE", ROOT . '/core');
 define("PUBLIC", ROOT . '/public');
 
 require_once CORE . "/functions.php";
-require_once CONTROLLERS . "/index.php";
+
+$url = trim($_SERVER['REQUEST_URI'], '/');
+
+if ($url === '' || $url === 'index.php') {
+    require_once CONTROLLERS . "/index.php";
+
+} else if ($url === 'contacts.php') {
+    require_once CONTROLLERS . "/contacts.php";
+} else {
+    http_response_code(404);
+    require_once VIEWS . "/errors/404.tmpl.php";
+}
 
 
 ?>
