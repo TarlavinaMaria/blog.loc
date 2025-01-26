45582784d590b6959a1bc01040c552c6ee04ebf7
Мария Тарлавина <tarlavina50@mail.ru>
2025-01-26 18:51:52 +0700

    DZ +

--- app/controllers/post.php
+++ app/controllers/post.php
@@ -1,6 +1,8 @@
 <?php
 $id = (int) $_GET['id'] ?? 0;
+$action = $_POST['action'] ?? null;
 
+// Получение данных поста
 $post = $db->query("SELECT * FROM posts WHERE post_id = ?", [$id])->findOrAbort();
 $most_popular_posts = $db->query("SELECT * FROM posts ORDER BY rating DESC LIMIT 5;")->findAll();
 
@@ -9,6 +11,12 @@ $header = $post['title'];
 $content = $post['content'];
 $rating = $post['rating'];
 
+// Обновляем данные рейтинга
+if ($action === 'up') {
+    $db->query("UPDATE posts SET rating = rating + 1 WHERE post_id = ?", [$id]);
+} elseif ($action === 'down') {
+    $db->query("UPDATE posts SET rating = rating - 1 WHERE post_id = ?", [$id]);
+}
 
 require_once(VIEWS . '/post.tmpl.php');
 ?>
--- app/views/post.tmpl.php
+++ app/views/post.tmpl.php
@@ -1,4 +1,6 @@
 <?php require(COMPONENTS . "/header.php"); ?>
+
+
 <main class="main py-3">
     <div class="container">
         <div class="row">
@@ -8,6 +10,11 @@
             <h3><?= $header ?? "" ?></h3>
             <p> <?= $content ?></p>
             <p> Rating: <?= $rating ?>/10</p>
+            <form method="POST">
+                <input type="hidden" name="post_id" value="<?= $id ?>">
+                <button type="submit" name="action" value="up">👍</button>
+                <button type="submit" name="action" value="down">👎</button>
+            </form>
         </div>
     </div>
 </main>
