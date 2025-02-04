b6d779d5927d36ebbefbdfef51309244bd2be28f
Мария Тарлавина <tarlavina50@mail.ru>
2025-02-04 21:46:30 +0700

    +

--- app/controllers/login.php
+++ app/controllers/login.php
@@ -1,8 +1,8 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-    $email = trim($_POST['email']);
-    $password = trim($_POST['password']);
+    $email = htmlspecialchars(trim($_POST['email']));
+    $password = htmlspecialchars(trim($_POST['password']));
 
     // Валидация ввода
     if (empty($email) || empty($password)) {
